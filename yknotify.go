eb09ecf3e80b53ef11e8886421a5de47379df677
noperator <noper@tor.fyi>
2025-05-28 13:23:07 -0400

    filter on YubiKey-associated HID clients

--- yknotify.go
+++ yknotify.go
@@ -68,6 +68,7 @@ func streamLogs() error {
 
 	state := &TouchState{}
 	scanner := bufio.NewScanner(stdout)
+	yubiKeyClients := make(map[string]bool)
 
 	go func() {
 		ticker := time.NewTicker(time.Second)
@@ -85,8 +86,30 @@ func streamLogs() error {
 		switch {
 		case entry.ProcessImagePath == "/kernel" &&
 			strings.HasSuffix(entry.SenderImagePath, "IOHIDFamily"):
-			state.fido2Needed = strings.Contains(entry.EventMessage, "IOHIDLibUserClient:0x") &&
-				strings.HasSuffix(entry.EventMessage, "startQueue")
+			msg := entry.EventMessage
+
+			// e.g., AppleUserUSBHostHIDDevice:0x100000c81 open by IOHIDLibUserClient:0x10016f869 (0x1)
+			// Other HID types (e.g., AppleUSBTopCaseHIDDriver) do not correspond to YubiKey and will trigger a false positive.
+			if strings.Contains(msg, "AppleUserUSBHostHIDDevice:") && strings.Contains(msg, "open by IOHIDLibUserClient:") {
+				parts := strings.Split(msg, " open by ")
+				if len(parts) == 2 {
+					clientID := strings.Split(parts[1], " ")[0]
+					yubiKeyClients[clientID] = true
+				}
+			}
+
+			// e.g., IOHIDLibUserClient:0x10016f869 startQueue
+			// Only trigger FIDO2 for tracked YubiKey clients.
+			if strings.HasSuffix(msg, "startQueue") {
+				clientID := strings.Split(msg, " ")[0]
+				state.fido2Needed = yubiKeyClients[clientID]
+			} else if strings.HasSuffix(msg, "stopQueue") {
+				clientID := strings.Split(msg, " ")[0]
+				if yubiKeyClients[clientID] {
+					state.fido2Needed = false
+				}
+			}
+
 		case strings.HasSuffix(entry.ProcessImagePath, "usbsmartcardreaderd") &&
 			strings.HasSuffix(entry.Subsystem, "CryptoTokenKit"):
 			state.openPGPNeeded = entry.EventMessage == "Time extension received"
